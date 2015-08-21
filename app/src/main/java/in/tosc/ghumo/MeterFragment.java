ea03e9608c8fcdea6d7aeffe33764edf990e2f3e
Prempal Singh <prempal.42@gmail.com>
2015-08-22 00:11:07 +0530

    added fragments

---
+++ app/src/main/java/in/tosc/ghumo/MeterFragment.java
@@ -0,0 +1,33 @@
+package in.tosc.ghumo;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MeterFragment extends Fragment {
+
+
+    public MeterFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_meter, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
