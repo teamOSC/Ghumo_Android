cb3f7b1162dc154ad459bcb9f2711c35d1f04c77
Arnav Gupta <championswimmer@gmail.com>
2015-08-22 03:49:08 +0530

    pojo for fares

---
+++ app/src/main/java/in/tosc/ghumo/Fare.java
@@ -0,0 +1,70 @@
+package in.tosc.ghumo;
+
+/**
+ * Created by championswimmer on 22/8/15.
+ */
+public class Fare {
+    int waiting_charges;
+    String city;
+    int min_dist;
+    int booking_fee;
+    String operator;
+    float fare_per_km;
+    float min_fare;
+
+    public int getWaiting_charges() {
+        return waiting_charges;
+    }
+
+    public void setWaiting_charges(int waiting_charges) {
+        this.waiting_charges = waiting_charges;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getMin_dist() {
+        return min_dist;
+    }
+
+    public void setMin_dist(int min_dist) {
+        this.min_dist = min_dist;
+    }
+
+    public int getBooking_fee() {
+        return booking_fee;
+    }
+
+    public void setBooking_fee(int booking_fee) {
+        this.booking_fee = booking_fee;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public float getFare_per_km() {
+        return fare_per_km;
+    }
+
+    public void setFare_per_km(float fare_per_km) {
+        this.fare_per_km = fare_per_km;
+    }
+
+    public float getMin_fare() {
+        return min_fare;
+    }
+
+    public void setMin_fare(float min_fare) {
+        this.min_fare = min_fare;
+    }
+}
