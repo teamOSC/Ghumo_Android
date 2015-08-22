e03b62d8d57f50b64967f89414971acb291be601
naman14 <namandwivedi14@gmail.com>
2015-08-22 14:20:17 +0530

    rating detail ui changes

--- app/src/main/java/in/tosc/ghumo/RatingDetailActivity.java
+++ app/src/main/java/in/tosc/ghumo/RatingDetailActivity.java
@@ -3,17 +3,24 @@ package in.tosc.ghumo;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by naman on 22/08/15.
  */
 public class RatingDetailActivity extends AppCompatActivity {
 
-    RatingBar ratingBar;
+    RatingBar ratingBar,ratingBarUser;
+    View submitFeedback;
+    TextView submitText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +28,18 @@ public class RatingDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rating_detail);
 
         ratingBar=(RatingBar) findViewById(R.id.rating);
-        ratingBar.setRating(4.2f);
+        ratingBarUser=(RatingBar) findViewById(R.id.rating_user);
+        submitFeedback=findViewById(R.id.submit_feedback_button);
+        submitText=(TextView) findViewById(R.id.submit_text);
+
+        ratingBar.setRating(4.4f);
+        ratingBarUser.setRating(0);
 
         LayerDrawable stars = (LayerDrawable) ratingBar.getProgressDrawable();
         stars.getDrawable(2).setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.SRC_ATOP);
+
+        LayerDrawable stars1 = (LayerDrawable) ratingBarUser.getProgressDrawable();
+        stars1.getDrawable(2).setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.SRC_ATOP);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -32,6 +47,22 @@ public class RatingDetailActivity extends AppCompatActivity {
         final ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
         ab.setTitle("Driver Ratings");
+
+        submitFeedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submitText.setText("Submitting Feedback...");
+                submitText.setCompoundDrawables(null,null,null,null);
+                Handler handler=new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(RatingDetailActivity.this,"Feedback Submitted",Toast.LENGTH_SHORT).show();
+                        submitText.setText("Submit Feedback");
+                    }
+                },1500);
+            }
+        });
 
     }
 }
