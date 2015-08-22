258e3d945497862d686fccf9e7d4f082dc272ffc
naman14 <namandwivedi14@gmail.com>
2015-08-22 12:50:31 +0530

    timely textview

--- app/src/main/java/in/tosc/ghumo/MeterFragment.java
+++ app/src/main/java/in/tosc/ghumo/MeterFragment.java
@@ -3,13 +3,20 @@ package in.tosc.ghumo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.github.adnansm.timelytextview.TimelyView;
 
 import in.tosc.ghumo.widgets.ScrollingImageView;
 
@@ -18,6 +25,9 @@ public class MeterFragment extends android.support.v4.app.Fragment {
     Button start, stop, reset;
     ScrollingImageView image1,image2;
     View scrollingFrame;
+    ImageView van;
+    TimelyView timelyView;
+    com.nineoldandroids.animation.ObjectAnimator objectAnimator;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,6 +37,8 @@ public class MeterFragment extends android.support.v4.app.Fragment {
         scrollingFrame=rootView.findViewById(R.id.scrollingFrame);
         image1=(ScrollingImageView) rootView.findViewById(R.id.scollingImage1);
         image2=(ScrollingImageView) rootView.findViewById(R.id.scollingImage2);
+        van=(ImageView) rootView.findViewById(R.id.van);
+        timelyView=(TimelyView) rootView.findViewById(R.id.timelyView);
 
         Toolbar toolbar = (Toolbar) rootView.findViewById(R.id.toolbar);
         ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
@@ -58,6 +70,16 @@ public class MeterFragment extends android.support.v4.app.Fragment {
             }
         });
 
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                objectAnimator=timelyView.animate(8,9);
+                objectAnimator.setDuration(1000);
+                objectAnimator.start();
+            }
+        },2000);
+
         return rootView;
     }
 
@@ -70,6 +92,16 @@ public class MeterFragment extends android.support.v4.app.Fragment {
         scrollingFrame.setVisibility(View.VISIBLE);
         image1.start();
         image2.start();
+        Animation mAnimation = new TranslateAnimation(
+                TranslateAnimation.ABSOLUTE, 0f,
+                TranslateAnimation.ABSOLUTE, 0f,
+                TranslateAnimation.RELATIVE_TO_PARENT, 0.1f,
+                TranslateAnimation.RELATIVE_TO_PARENT, 0f);
+        mAnimation.setDuration(200);
+        mAnimation.setRepeatCount(-1);
+        mAnimation.setRepeatMode(Animation.REVERSE);
+        mAnimation.setInterpolator(new LinearInterpolator());
+        van.setAnimation(mAnimation);
     }
 
     private void stopMovingAnimation(){
